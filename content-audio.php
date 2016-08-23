63fc4c1614c33bf905ed23f68755d3705ab73606
EXL <exlmotodev@gmail.com>
2016-08-23 21:15:15 +0000

    Reformat articles block

--- content-audio.php
+++ content-audio.php
@@ -13,9 +13,6 @@
 
 	<header class="entry-header">
 		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-		<div class="entry-meta">
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-		</div><!-- .entry-meta -->
 		<?php
 			endif;
 
@@ -57,6 +54,10 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+
+	<div class="entry-header">
+		<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+	</div><!-- .entry-header -->
 
 	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
 </article><!-- #post-## -->
--- content.php
+++ content.php
@@ -15,9 +15,6 @@
 
 	<header class="entry-header">
 		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-		<div class="entry-meta">
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-		</div>
 		<?php
 			endif;
 
@@ -66,6 +63,10 @@
 		?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
+
+	<div class="entry-header">
+		<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+	</div>
 
 	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
 </article><!-- #post-## -->
