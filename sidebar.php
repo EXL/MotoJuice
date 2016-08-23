67fe5e0d1fc00c6c6d3b6808486efefd1bb775bd
EXL <exlmotodev@gmail.com>
2016-08-23 16:57:15 +0000

    Disable Site Description

--- sidebar.php
+++ sidebar.php
@@ -8,13 +8,6 @@
  */
 ?>
 <div id="secondary">
-	<?php
-		$description = get_bloginfo( 'description', 'display' );
-		if ( ! empty ( $description ) ) :
-	?>
-	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
-	<?php endif; ?>
-
 	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
 	<nav role="navigation" class="navigation site-navigation secondary-navigation">
 		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
