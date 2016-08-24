a1a7b8c941f4d57a5e661e59e165f0756abc6642
EXL <exlmotodev@gmail.com>
2016-08-24 16:07:21 +0000

    Reformat footer and add RSS Icon

--- footer.php
+++ footer.php
@@ -16,10 +16,23 @@
 
 			<?php get_sidebar( 'footer' ); ?>
 
-			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+			<a class="rss-sign" href="<?php bloginfo('atom_url'); ?>" title="<?php printf( __( 'RSS Atom Feed', 'twentyfourteen' ) ); ?>" target="_blank">
+				<span><?php printf( __( 'RSS Atom', 'twentyfourteen' ) ); ?></span>
+			</a><!-- .rss-sign -->
+
+			<div class="copyright-sign">
+				<?php printf( __( 'Copyright © 2010-%s. All Rights Reserved', 'twentyfourteen' ), date('Y') ); ?>
+			</div><!--.copyright-sign-->
+
+			<div class="design-sign">
+				<?php printf( __( 'Designed by ', 'twentyfourteen' ) ); ?>
+				<a href="<?php echo esc_url( __( 'https://exlmoto.ru/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'EXL', 'twentyfourteen' ) ); ?></a>
+			</div><!--.design-sign-->
+
+			<div class="theme-sign">
+				<?php printf( __( 'Theme: ', 'twentyfourteen' ) ); ?>
+				<a href="<?php echo esc_url( __( 'https://github.com/EXL/MotoJuice', 'twentyfourteen' ) ); ?>"><?php printf( __( 'MotoJuice', 'twentyfourteen' ) ); ?></a>
+			</div><!--.theme-sign-->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
