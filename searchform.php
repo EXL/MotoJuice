97d0c969cbc1ef8a9de4ac937803651ef9f863d3
EXL <exlmotodev@gmail.com>
2016-08-24 19:55:51 +0000

    Fix search bug

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
 			placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>"
 			title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 		<span class="button-wrapper">
-			<input class="button-search" type="submit" name="search" value="<?php  echo esc_attr(__('Search', 'twentyfourteen')); ?>" />
+			<input class="button-search" type="submit" value="<?php  echo esc_attr(__('Search', 'twentyfourteen')); ?>" />
 		</span>
 	</div>
 </form>
