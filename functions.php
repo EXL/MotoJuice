34b376be2c2262c1ea7db6110f66b50966ce5618
EXL <exlmotodev@gmail.com>
2016-08-24 18:50:29 +0000

    Fix page title

--- functions.php
+++ functions.php
@@ -490,12 +490,12 @@ function twentyfourteen_wp_title( $title, $sep ) {
 	// Add the site description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title = "$title $sep $site_description";
+		$title = "$title";
 	}
 
 	// Add a page number if necessary.
 	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentyfourteen' ), max( $paged, $page ) );
+		$title = sprintf( __( 'Page %s', 'twentyfourteen' ), max( $paged, $page ) ) . " $sep $title";
 	}
 
 	return $title;
--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<!-- Note: wp_title filtered in functions.php -->
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
