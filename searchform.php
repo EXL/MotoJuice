a03c91bc9f2cc2d4d17f7204be1c6e6aa8f037e2
EXL <exlmotodev@gmail.com>
2016-08-24 18:36:30 +0000

    Add custom search form

---
+++ searchform.php
@@ -0,0 +1,11 @@
+<form method="get" name="searchform" action="<?php bloginfo('url'); ?>/">
+	<div class="search">
+		<input class="search-input" name="s" type="text" autofocus
+			value="<?php echo esc_attr(get_search_query()); ?>"
+			placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>"
+			title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+		<span class="button-wrapper">
+			<input class="button-search" type="submit" name="search" value="<?php  echo esc_attr(__('Search', 'twentyfourteen')); ?>" />
+		</span>
+	</div>
+</form>
