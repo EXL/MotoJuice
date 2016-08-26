446d9ea4ede4084a30bdf875c45ab9ad5c1167e7
EXL <exlmotodev@gmail.com>
2016-08-26 22:07:24 +0000

    Improve widget and fix bugs

--- header.php
+++ header.php
@@ -43,21 +43,51 @@
 			</a>
 
 			<div class="headerWidget" id="headerWidgetId">
-				<a href="http://exlmoto.ru/spout-droid/" title="Spout">
-					<img class="imgElem" id="imgElem0" src="<?php echo get_stylesheet_directory_uri(); ?>/images/widget/0.png" />
-				</a>
-				<a href="http://exlmoto.ru/kenlab3d-droid/" title="Ken’s Labyrinth">
-					<img class="imgElem" id="imgElem1" src="<?php echo get_stylesheet_directory_uri(); ?>/images/widget/1.png" />
-				</a>
-				<a href="http://exlmoto.ru/astrosmash-droid/" title="AstroSmash">
-					<img class="imgElem" id="imgElem2" src="<?php echo get_stylesheet_directory_uri(); ?>/images/widget/2.png" />
-				</a>
-				<a href="http://exlmoto.ru/snooder21-droid/" title="Snooder 21">
-					<img class="imgElem" id="imgElem3" src="<?php echo get_stylesheet_directory_uri(); ?>/images/widget/3.png" />
-				</a>
-				<a href="http://exlmoto.ru/bezier-clock/" title="Bezier Clock">
-					<img class="imgElem" id="imgElem4" src="<?php echo get_stylesheet_directory_uri(); ?>/images/widget/4.png" />
-				</a>
+				<?php
+					$imagesCount = 5;
+					$themeDir = get_stylesheet_directory_uri();
+					$imageLinks = array(
+						"<a href=\"http://exlmoto.ru/spout-droid/\" title=\"Spout\">" .
+							"<img class=\"imgElem\" id=\"imgElem0\" src=\"" . $themeDir . "/images/widget/0.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/kenlab3d-droid/\" title=\"Ken’s Labyrinth\">" .
+							"<img class=\"imgElem\" id=\"imgElem1\" src=\"" . $themeDir . "/images/widget/1.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/astrosmash-droid/\" title=\"AstroSmash\">" .
+							"<img class=\"imgElem\" id=\"imgElem2\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/snooder21-droid/\" title=\"Snooder 21\">" .
+							"<img class=\"imgElem\" id=\"imgElem3\" src=\"" . $themeDir . "/images/widget/3.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
+							"<img class=\"imgElem\" id=\"imgElem4\" src=\"" . $themeDir . "/images/widget/4.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
+							"<img class=\"imgElem\" id=\"imgElem5\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
+						"</a>",
+						"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
+							"<img class=\"imgElem\" id=\"imgElem6\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
+						"</a>"
+					);
+
+					$lengthArr = count($imageLinks);
+					$randNum = rand(0, $lengthArr - 1);
+					$offset = ($randNum + $imagesCount) - $lengthArr;
+
+					if ($randNum <= $lengthArr - $imagesCount) {
+						for ($i = $randNum; $i < $randNum + $imagesCount; $i++) {
+							echo $imageLinks[$i];
+						}
+					} else {
+						for ($j = $randNum; $j < $lengthArr; $j++) {
+							echo $imageLinks[$j];
+						}
+
+						for ($k = 0; $k < $offset; $k++) {
+							echo $imageLinks[$k];
+						}
+					}
+				?>
 
 				<!-- Please use the PoEdit program for changing the descriptions. Don't edit this labels manually!
 					 PoEdit site: https://poedit.net/ -->
