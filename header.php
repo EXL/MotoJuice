d54b2ebc5684cc6c941e4ffee11b4c1e17bcf3a2
EXL <exlmotodev@gmail.com>
2016-08-26 18:01:26 +0000

    Fix header image and layout

--- header.php
+++ header.php
@@ -33,20 +33,14 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/header-image.png" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-		</a>
-	</div>
-	<?php endif; ?>
-
 	<header id="masthead" class="site-header" role="banner">
 
 	<hr class="widgetBreaker">
 
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/header-logo.png">
+			</a>
 
 			<div class="headerWidget" id="headerWidgetId">
 				<a href="http://exlmoto.ru/spout-droid/" title="Spout">
