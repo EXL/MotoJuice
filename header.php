cce4d0c50d74c0daf42937dff5cf3f60a58a5ceb
EXL <exlmotodev@gmail.com>
2016-10-14 03:55:27 +0000

    Hide header button desc

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 			<div class="secondDescription" id="imgDescSecond"><?php printf( __( 'Welcome!', 'moto-juice' ) ); ?></div>
 
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<button class="menu-toggle" title="<?php printf( __( 'Show or hide menu', 'moto-juice' ) ); ?>"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+				<button class="menu-toggle" title="<?php printf( __( 'Show or hide menu', 'moto-juice' ) ); ?>"></button>
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
 				<div id="primary-menu" class="nav-menu" aria-expanded="true">
 					<?php dynamic_sidebar( 'sidebar-1' ); ?>
