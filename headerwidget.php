10d1f3f61e5475c66a7635838119f399f7321869
EXL <exlmotodev@gmail.com>
2017-04-26 17:50:08 +0700

    Test new HeaderWidget #1

--- headerwidget.php
+++ headerwidget.php
@@ -11,33 +11,51 @@
 		echo $arrw;
 
 		$imageLinks = array(
-			"<a href=\"http://exlmoto.ru/spout-droid/\" title=\"Spout\">" .
-				"<img class=\"imgElem\" id=\"imgElem0\" src=\"" . $themeDir . "/images/widget/0.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/kenlab3d-droid/\" title=\"Ken's Labyrinth\">" .
-				"<img class=\"imgElem\" id=\"imgElem1\" src=\"" . $themeDir . "/images/widget/1.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/astrosmash-droid/\" title=\"AstroSmash\">" .
-				"<img class=\"imgElem\" id=\"imgElem2\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/snooder21-droid/\" title=\"Snooder 21\">" .
-				"<img class=\"imgElem\" id=\"imgElem3\" src=\"" . $themeDir . "/images/widget/3.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
-				"<img class=\"imgElem\" id=\"imgElem4\" src=\"" . $themeDir . "/images/widget/4.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/nxengine/\" title=\"Cave Story\">" .
-				"<img class=\"imgElem\" id=\"imgElem5\" src=\"" . $themeDir . "/images/widget/5.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/writing-telegram-bots/\" title=\"Gadget Hackwrench\">" .
-				"<img class=\"imgElem\" id=\"imgElem6\" src=\"" . $themeDir . "/images/widget/6.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/new-updates-and-tools/#1\" title=\"Synergy Calls\">" .
-				"<img class=\"imgElem\" id=\"imgElem7\" src=\"" . $themeDir . "/images/widget/7.png\" />" .
-			"</a>",
-			"<a href=\"http://exlmoto.ru/adamant-armor-affection-adventure-droid/\" title=\"Adamant Armor Affection Adventure\">" .
-				"<img class=\"imgElem\" id=\"imgElem8\" src=\"" . $themeDir . "/images/widget/8.png\" />" .
-			"</a>"
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/spout-droid/\" title=\"Spout\">" .
+					"<img class=\"imgElem\" id=\"imgElem0\" src=\"" . $themeDir . "/images/widget/0.png\" />" .
+				"</a>" . __( '<b>Project 1</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/kenlab3d-droid/\" title=\"Ken's Labyrinth\">" .
+					"<img class=\"imgElem\" id=\"imgElem1\" src=\"" . $themeDir . "/images/widget/1.png\" />" .
+				"</a>" . __( '<b>Project 2</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/astrosmash-droid/\" title=\"AstroSmash\">" .
+					"<img class=\"imgElem\" id=\"imgElem2\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
+				"</a>" . __( '<b>Project 3</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/snooder21-droid/\" title=\"Snooder 21\">" .
+					"<img class=\"imgElem\" id=\"imgElem3\" src=\"" . $themeDir . "/images/widget/3.png\" />" .
+				"</a>" . __( '<b>Project 4</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
+					"<img class=\"imgElem\" id=\"imgElem4\" src=\"" . $themeDir . "/images/widget/4.png\" />" .
+				"</a>" . __( '<b>Project 5</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/nxengine/\" title=\"Cave Story\">" .
+					"<img class=\"imgElem\" id=\"imgElem5\" src=\"" . $themeDir . "/images/widget/5.png\" />" .
+				"</a>" . __( '<b>Project 6</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/writing-telegram-bots/\" title=\"Gadget Hackwrench\">" .
+					"<img class=\"imgElem\" id=\"imgElem6\" src=\"" . $themeDir . "/images/widget/6.png\" />" .
+				"</a>" . __( '<b>Project 7</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/new-updates-and-tools/#1\" title=\"Synergy Calls\">" .
+					"<img class=\"imgElem\" id=\"imgElem7\" src=\"" . $themeDir . "/images/widget/7.png\" />" .
+				"</a>" . __( '<b>Project 8</b> Description.', 'moto-juice' ) .
+			"</div>",
+			"<div class=\"projectIcon\">" .
+				"<a href=\"http://exlmoto.ru/adamant-armor-affection-adventure-droid/\" title=\"Adamant Armor Affection Adventure\">" .
+					"<img class=\"imgElem\" id=\"imgElem8\" src=\"" . $themeDir . "/images/widget/8.png\" />" .
+				"</a>" . __( '<b>Project 9</b> Description.', 'moto-juice' ) .
+			"</div>"
 		);
 
 		$lengthArr = count($imageLinks);
@@ -63,23 +81,26 @@
 			 * Please use the PoEdit program for changing the descriptions. Don't edit this labels manually!
 			 * PoEdit site: https://poedit.net/
 			 */
-	?>
 
-	<div class="imgDescription" id="imgDesc0"><?php printf( __( '<b>Project 1</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc1"><?php printf( __( '<b>Project 2</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc2"><?php printf( __( '<b>Project 3</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc3"><?php printf( __( '<b>Project 4</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc4"><?php printf( __( '<b>Project 5</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc5"><?php printf( __( '<b>Project 6</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc6"><?php printf( __( '<b>Project 7</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc7"><?php printf( __( '<b>Project 8</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc8"><?php printf( __( '<b>Project 9</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc9"><?php printf( __( '<b>Project 10</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc10"><?php printf( __( '<b>Project 11</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc11"><?php printf( __( '<b>Project 12</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc12"><?php printf( __( '<b>Project 13</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc13"><?php printf( __( '<b>Project 14</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc14"><?php printf( __( '<b>Project 15</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc15"><?php printf( __( '<b>Project 16</b> Description.', 'moto-juice' ) ); ?></div>
-	<div class="imgDescription" id="imgDesc99"><?php printf( __( '<b>Project 99</b> Description.', 'moto-juice' ) ); ?></div>
+			 __( '<b>Project 1</b> Description.', 'moto-juice' );
+			 __( '<b>Project 2</b> Description.', 'moto-juice' );
+			 __( '<b>Project 3</b> Description.', 'moto-juice' );
+			 __( '<b>Project 4</b> Description.', 'moto-juice' );
+			 __( '<b>Project 5</b> Description.', 'moto-juice' );
+			 __( '<b>Project 6</b> Description.', 'moto-juice' );
+			 __( '<b>Project 7</b> Description.', 'moto-juice' );
+			 __( '<b>Project 8</b> Description.', 'moto-juice' );
+			 __( '<b>Project 9</b> Description.', 'moto-juice' );
+			 __( '<b>Project 10</b> Description.', 'moto-juice' );
+			 __( '<b>Project 11</b> Description.', 'moto-juice' );
+			 __( '<b>Project 12</b> Description.', 'moto-juice' );
+			 __( '<b>Project 13</b> Description.', 'moto-juice' );
+			 __( '<b>Project 14</b> Description.', 'moto-juice' );
+			 __( '<b>Project 15</b> Description.', 'moto-juice' );
+			 __( '<b>Project 16</b> Description.', 'moto-juice' );
+			 __( '<b>Project 17</b> Description.', 'moto-juice' );
+			 __( '<b>Project 18</b> Description.', 'moto-juice' );
+			 __( '<b>Project 19</b> Description.', 'moto-juice' );
+			 __( '<b>Project 20</b> Description.', 'moto-juice' );
+	?>
 </div>
