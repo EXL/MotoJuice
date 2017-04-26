f74fbf684bf03dd33b7cc27c78d05ac1bddfa42a
EXL <exlmotodev@gmail.com>
2017-04-26 18:40:33 +0700

    Test translations

--- headerwidget.php
+++ headerwidget.php
@@ -2,6 +2,29 @@
 	<?php
 		$imagesCount = 3;
 		$themeDir = get_stylesheet_directory_uri();
+
+		// Please use the PoEdit program for changing the descriptions. Don't edit this labels manually!
+		// PoEdit site: https://poedit.net/
+		$p1  = __( '<b>Project 1</b> Description.', 'moto-juice' );
+		$p2  = __( '<b>Project 2</b> Description.', 'moto-juice' );
+		$p3  = __( '<b>Project 3</b> Description.', 'moto-juice' );
+		$p4  = __( '<b>Project 4</b> Description.', 'moto-juice' );
+		$p5  = __( '<b>Project 5</b> Description.', 'moto-juice' );
+		$p6  = __( '<b>Project 6</b> Description.', 'moto-juice' );
+		$p7  = __( '<b>Project 7</b> Description.', 'moto-juice' );
+		$p8  = __( '<b>Project 8</b> Description.', 'moto-juice' );
+		$p9  = __( '<b>Project 9</b> Description.', 'moto-juice' );
+		$p10 = __( '<b>Project 10</b> Description.', 'moto-juice' );
+		$p11 = __( '<b>Project 11</b> Description.', 'moto-juice' );
+		$p12 = __( '<b>Project 12</b> Description.', 'moto-juice' );
+		$p13 = __( '<b>Project 13</b> Description.', 'moto-juice' );
+		$p14 = __( '<b>Project 14</b> Description.', 'moto-juice' );
+		$p15 = __( '<b>Project 15</b> Description.', 'moto-juice' );
+		$p16 = __( '<b>Project 16</b> Description.', 'moto-juice' );
+		$p17 = __( '<b>Project 17</b> Description.', 'moto-juice' );
+		$p18 = __( '<b>Project 18</b> Description.', 'moto-juice' );
+		$p19 = __( '<b>Project 19</b> Description.', 'moto-juice' );
+		$p20 = __( '<b>Project 20</b> Description.', 'moto-juice' );
 		$allProjects = __( 'Go to All Projects Page', 'moto-juice' );
 
 		// Arrow
@@ -14,47 +37,47 @@
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/spout-droid/\" title=\"Spout\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/0.png\" />" .
-				"</a>" . __( '<b>Project 1</b> Description.', 'moto-juice' ) .
+				"</a>" . $p1 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/kenlab3d-droid/\" title=\"Ken's Labyrinth\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/1.png\" />" .
-				"</a>" . __( '<b>Project 2</b> Description.', 'moto-juice' ) .
+				"</a>" . $p2 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/astrosmash-droid/\" title=\"AstroSmash\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/2.png\" />" .
-				"</a>" . __( '<b>Project 3</b> Description.', 'moto-juice' ) .
+				"</a>" . $p3 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/snooder21-droid/\" title=\"Snooder 21\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/3.png\" />" .
-				"</a>" . __( '<b>Project 4</b> Description.', 'moto-juice' ) .
+				"</a>" . $p4 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/bezier-clock/\" title=\"Bezier Clock\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/4.png\" />" .
-				"</a>" . __( '<b>Project 5</b> Description.', 'moto-juice' ) .
+				"</a>" . $p5 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/nxengine/\" title=\"Cave Story\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/5.png\" />" .
-				"</a>" . __( '<b>Project 6</b> Description.', 'moto-juice' ) .
+				"</a>" . $p6 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/writing-telegram-bots/\" title=\"Gadget Hackwrench\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/6.png\" />" .
-				"</a>" . __( '<b>Project 7</b> Description.', 'moto-juice' ) .
+				"</a>" . $p7 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/new-updates-and-tools/#1\" title=\"Synergy Calls\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/7.png\" />" .
-				"</a>" . __( '<b>Project 8</b> Description.', 'moto-juice' ) .
+				"</a>" . $p8 .
 			"</div>",
 			"<div class=\"projectIcon\">" .
 				"<a href=\"http://exlmoto.ru/adamant-armor-affection-adventure-droid/\" title=\"Adamant Armor Affection Adventure\">" .
 					"<img class=\"imgElem\" align=\"left\" src=\"" . $themeDir . "/images/widget/8.png\" />" .
-				"</a>" . __( '<b>Project 9</b> Description.', 'moto-juice' ) .
+				"</a>" . $p9 .
 			"</div>"
 		);
 
@@ -75,32 +98,5 @@
 				echo $imageLinks[$k];
 			}
 		}
-	?>
-
-	<?php 	/*
-			 * Please use the PoEdit program for changing the descriptions. Don't edit this labels manually!
-			 * PoEdit site: https://poedit.net/
-			 */
-
-			 __( '<b>Project 1</b> Description.', 'moto-juice' );
-			 __( '<b>Project 2</b> Description.', 'moto-juice' );
-			 __( '<b>Project 3</b> Description.', 'moto-juice' );
-			 __( '<b>Project 4</b> Description.', 'moto-juice' );
-			 __( '<b>Project 5</b> Description.', 'moto-juice' );
-			 __( '<b>Project 6</b> Description.', 'moto-juice' );
-			 __( '<b>Project 7</b> Description.', 'moto-juice' );
-			 __( '<b>Project 8</b> Description.', 'moto-juice' );
-			 __( '<b>Project 9</b> Description.', 'moto-juice' );
-			 __( '<b>Project 10</b> Description.', 'moto-juice' );
-			 __( '<b>Project 11</b> Description.', 'moto-juice' );
-			 __( '<b>Project 12</b> Description.', 'moto-juice' );
-			 __( '<b>Project 13</b> Description.', 'moto-juice' );
-			 __( '<b>Project 14</b> Description.', 'moto-juice' );
-			 __( '<b>Project 15</b> Description.', 'moto-juice' );
-			 __( '<b>Project 16</b> Description.', 'moto-juice' );
-			 __( '<b>Project 17</b> Description.', 'moto-juice' );
-			 __( '<b>Project 18</b> Description.', 'moto-juice' );
-			 __( '<b>Project 19</b> Description.', 'moto-juice' );
-			 __( '<b>Project 20</b> Description.', 'moto-juice' );
 	?>
 </div>
